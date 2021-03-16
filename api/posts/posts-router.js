460cf6aa0e2a8b245b21b8bca1e756ee09459b3c
AlexAndrew Mallory <pytormal@live.com>
2021-03-16 14:16:44 -0700

    attempted step 2

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,18 +4,39 @@ const express = require("express");
 const router = express.Router();
 const Post = require("./posts-model");
 
-
-// 1 [GET] /api/posts Completed
+// 1 [GET] /api/posts
+// Completed
 router.get("/", async (req, res) => {
-try {
+  try {
     const post = await Post.find();
-    res.json(post)
-} catch (error) {
-    console.log(error)
+    res.json(post);
+  } catch (error) {
+    console.log(error);
     res
       .status(500)
       .json({ message: "The posts information could not be retrieved" });
-    
-}
-}); 
+  }
+});
+
+// 2 [GET] /api/posts/:id
+// Incomplete: id in url does not show post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(id);
+    if (post) {
+      res.json(post);
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    }
+  }
+  catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" });
+  }
+});
+
 module.exports = router;
